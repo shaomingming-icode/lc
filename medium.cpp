3f3cc61efa5a7359e77138c999c8ad9dc3696fcf
mm322 <275666594@qq.com>
2019-09-13 23:02:57 +0800

    Update medium.cpp

--- medium.cpp
+++ medium.cpp
@@ -45,7 +45,7 @@ Example 2 :
 	Explanation : You will always arrive at index 3 no matter what.Its maximum
 	jump length is 0, which makes it impossible to reach the last index.
 
-̰���㷨
+贪心算法
 
 bool canJump(vector<int>& nums) {
 	int reach = 0;
