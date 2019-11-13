caa133e4e0f10d191e1fbb949067967860f7306f
mm322 <275666594@qq.com>
2019-11-13 21:48:43 +0800

    Update medium_300-400.cpp

--- medium_300-400.cpp
+++ medium_300-400.cpp
@@ -28,7 +28,35 @@ void helper(string word, int pos, vector<string>& res) {
 }
 
 ---------------------------------------------------------------------
+
+322. Coin Change
+You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+Example 1:
+	Input: coins = [1, 2, 5], amount = 11
+	Output: 3 
+	Explanation: 11 = 5 + 5 + 1
+Example 2:
+	Input: coins = [2], amount = 3
+	Output: -1
+Note:
+You may assume that you have an infinite number of each kind of coin.
+
+int coinChange(vector<int>& coins, int amount) {
+    vector<int> values(amount + 1, amount + 1);
+    values[0] = 0;
+    for (int i = 0; i <= amount; i++) {
+        for (int j = 0; j < coins.size(); j++) {
+            if (i - coins[j] >= 0) {
+                values[i] = min(values[i - coins[j]] + 1, values[i]);
+            }
+        }
+    }
+    return values.back() > amount ? -1 : values.back();
+}
+
 ---------------------------------------------------------------------
+
 ---------------------------------------------------------------------
 ---------------------------------------------------------------------
 ---------------------------------------------------------------------
