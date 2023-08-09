c2b3ce047d5175da4c4af85db4ce4abfda5c263d
mm322 <275666594@qq.com>
2023-08-09 15:48:21 +0800

    2

--- c.cpp
+++ c.cpp
@@ -1,2 +1,10 @@
-#include  <iostream>
-             #include <vector>
+#include <iostream>
+#include <vector>
+
+int main(int argc, char* argv[])
+ {
+  int* ptr = new int[10];
+  delete ptr;
+  ptr[0] = 0;
+  return 0;
+}
