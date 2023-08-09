1be29dd0bc11bbbc0732ae883b69dc3f0ffa7aff
mm322 <275666594@qq.com>
2023-08-09 10:20:12 +0800

    modify

--- b.cpp
+++ b.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    int *ptr = new int[10];
-    delete ptr;
-    ptr[0] = 0;
-    return 0;
+  int* ptr = new int[10];
+  delete ptr;
+  ptr[0] = 0;
+  int i = 0;
+  if (i == 1) {
+  }
+  return 0;
 }
