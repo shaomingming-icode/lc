e92298dcf5b9f94a967252cc3ba9237276b9b0ba
mm322 <275666594@qq.com>
2023-12-11 15:41:00 +0800

    更新a

--- a.cpp
+++ a.cpp
@@ -2,8 +2,8 @@
 
 int main(int argc, char* argv[]) {
     int* ptr = new int[1];
-  int count = 0;
-  ptr[0] = 0;
-  delete[] ptr;
-  return 0;
+    int count = 0;
+    ptr[0] = 0;
+    delete[] ptr;
+    return 0;
 }
