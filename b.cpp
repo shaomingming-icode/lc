994b126f22791ad2c7c47f8497d366b7731dc9c5
mm322 <275666594@qq.com>
2023-12-19 14:59:12 +0800

    23434

--- b.cpp
+++ b.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-  int* ptr = new int[1];
+    int* ptr = new int[1];
   ptr[0] = 0;
   delete[] ptr;
   return 0;
--- c.cpp
+++
@@ -1,7 +0,0 @@
-
-int main(int argc, char* argv[]) {  // NOLINT
-    int* ptr = new int[1];
-    ptr[0] = 0;
-    delete[] ptr;
-    return 0;
-}
